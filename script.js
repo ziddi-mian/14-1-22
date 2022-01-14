d7ba7029280513003fbc045ba3ee0ae21b69c183
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-14 19:10:23 +0500

    js

--- script.js
+++ script.js
@@ -174,9 +174,10 @@ function diff_minutes(dt2, dt1)
   return Math.abs(Math.round(diff));
   
  }
- 
+
 
 dt1 = new Date(2014,10,2);
+
 dt2 = new Date(2014,10,3);
 console.log(diff_minutes(dt1, dt2));
 
